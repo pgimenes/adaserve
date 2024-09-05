61e7bd4387145c0656a49b338fa30fadac7e4974
Pedro Gimenes <pgimenes@outlook.com>
2024-09-05 14:08:19 +0100

    missing env var

--- experiments/benchmark_vllm.sh
+++ experiments/benchmark_vllm.sh
@@ -1,7 +1,9 @@
-export CHECKPOINT=$ADASERVE_CHECKPOINTS_PATH/nice-gpt2-1.5b
+export CHECKPOINT=$ADASERVE_CHECKPOINTS_PATH/nice-gpt2-4b
 export TENSOR_PARALLEL=$(nvidia-smi --list-gpus | wc -l)
-export BATCH_SIZE=8
+export BATCH_SIZE=32
 export SEQUENCE_LENGTH=128
+
+export VLLM_AUTOSHARDING_DATA_SIZE=1152
 
 python src/ada/vllm_bench.py \
     --model_name $CHECKPOINT \
