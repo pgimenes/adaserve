8d7aa8c525fce1d0d86022c00ed65e9e47e47a82
Pedro Gimenes <pgimenes@outlook.com>
2024-09-13 17:30:39 +0000

    baseline llama_3p1_8b azure-conv results

--- experiments/resharding_llama_3p1_8b.sh
+++ experiments/resharding_llama_3p1_8b.sh
@@ -3,7 +3,8 @@ export TENSOR_PARALLEL=4
 
 vllm serve \
     $CHECKPOINT \
-    --dynamic_resharding \
+    --enable_dynamic_resharding \
+    --api_key test \
     --load_format mase \
     --tensor_parallel_size $TENSOR_PARALLEL \
     --enforce_eager \
